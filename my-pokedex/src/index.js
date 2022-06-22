fcd0bb19d4a5b8da42ac1caa6b03510ee9e62d2e
Dawn Olivier <dmarieolivier@gmail.com>
2022-06-21 22:57:09 -0400

    started coding js file

---
+++ my-pokedex/src/index.js
@@ -0,0 +1,6 @@
+const BASE_URL = 'https://pokeapi.co/'
+window.addEventListener('DOMContentLoaded', () => {
+
+})
+const resultFromFetch = fetch('https://pokeapi.co/api/v2/pokemon/1')
+console.log(resultFromFetch)
