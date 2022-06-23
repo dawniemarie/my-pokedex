bd95e89c0f7cf25d91fe42197c277b893d8adf95
Dawn Olivier <dmarieolivier@gmail.com>
2022-06-23 00:13:42 -0400

    inserted poke sprite, added bg colors, capitalized poke name and types

--- my-pokedex/src/index.js
+++ my-pokedex/src/index.js
@@ -2,7 +2,7 @@ const BASE_URL = 'https://pokeapi.co/'
 window.addEventListener('DOMContentLoaded', () => {
 
 })
-
+// DOM selection
 const mainScreen = document.querySelector('.main-screen');
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
@@ -12,19 +12,49 @@ const pokeTypeTwo = document.querySelector('.poke-type-two');
 const pokeWeight = document.querySelector('.poke-weight');
 const pokeHeight = document.querySelector('.poke-height');
 
-console.log(pokeId);
+// console.log(pokeId);
+const pokeTypes = [
+    'normal', 'fighting', 'flying',
+    'poison', 'ground', 'rock', 
+    'bug', 'ghost', 'steel',
+    'fire', 'water', 'grass',
+    'electric', 'psychic', 'ice',
+    'dragon', 'dark', 'fairy'
+];
+
+// Functions
+const resetMainScreen = () => {
+    mainScreen.classList.remove('default');
+    for (const type of pokeTypes) {
+        // console.log(type)
+        mainScreen.classList.remove(type);
+    }
+}
+const capFirstLetter = (str) => str[0].toUpperCase() + str.substr(1);
+
 
 fetch('https://pokeapi.co/api/v2/pokemon/1')
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        mainScreen.classList.remove('default');
-        pokeName.textContent = data['name'];
-        pokeId.textContent = data['id'];
+        // console.log(data);
+        resetMainScreen();
+        // console.log(dataTypes);
+        const dataTypes = data['types'];
+        const dataFirstType = dataTypes[0];
+        const dataSecondType = dataTypes[1];
+        pokeTypeOne.textContent = capFirstLetter(dataFirstType['type']['name']);
+        if (dataSecondType) {
+            pokeTypeTwo.classList.remove('default');
+            pokeTypeTwo.textContent = capFirstLetter(dataSecondType['type']['name']);
+        } else {
+            pokeTypeTwo.classList.add('default');
+            pokeTypeTwo.textContent = '';
+        }
+        mainScreen.classList.add(dataFirstType['type']['name']);
+        pokeName.textContent = capFirstLetter(data['name']);
+        pokeId.textContent = "No." + data['id'];
         pokeWeight.textContent = data['weight'];
         pokeHeight.textContent = data['height'];
+        pokeFrontImage.src = data['sprites']['front_default'] || '';
 
-        const dataTypes = data['types'];
-        pokeTypeOne.textContent = dataTypes[0]['type']['name'];
-        // console.log(dataTypes);
     });
